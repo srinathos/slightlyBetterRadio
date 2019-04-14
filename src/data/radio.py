231a47130bd50468090a5271167a67c839334a98
Srinath Obla <sso8260@rit.edu>
2019-04-13 21:22:29 -0400

    Add support to read collection of links from a file

--- src/data/radio.py
+++ src/data/radio.py
@@ -20,10 +20,22 @@ def usage():
     print("Usage: python3 radio.py input_file1 input_file2 ... input_fileN")
 
 
+def read_links(filename):
+    links = list()
+    with open(filename) as f:
+        for line in f:
+            if line[0] != '#':
+                links.append(line)
+    return links
+
+
 def main():
     if len(sys.argv) < 2:
         usage()
     else:
+        # Reading file contents and sending for download
+        for filename in sys.argv[1:]:
+            links = read_links(filename)
         url = "https://www.youtube.com/watch?v=QnLMW08EdtE"
         audio_download(url)
 
